cf8a0354058427081cc0c37394985f3a55ce474a
Patrick Hayes <phayes2828@gmail.com>
2017-11-07 14:10:59 -0800

    completed

---
+++ MadLibs/MadLibViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MadLibViewController.swift
+//  MadLibs
+//
+//  Created by Patrick Hayes on 11/7/17.
+//  Copyright © 2017 Patrick Hayes. All rights reserved.
+//
+
+import UIKit
+
+class MadLibViewController: UIViewController {
+    
+    @IBOutlet weak var adjectiveField: UITextField!
+    @IBOutlet weak var verbField: UITextField!
+    @IBOutlet weak var verb2Field: UITextField!
+    @IBOutlet weak var nounField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var madlibLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        madlibLabel.text = "..."
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func submit(_ sender: UIStoryboardSegue) {
+        if sender.source is MadLibViewController{
+            if let controller = sender.source as? MadLibViewController{
+        
+                    let adjective = controller.adjectiveField.text!
+                    let verb1 = controller.verbField.text!
+                    let verb2 = controller.verb2Field.text!
+                    let noun = controller.nounField.text!
+                
+                    let str = "We are having a perfectly \(adjective) time now.  Later we will \(verb1) and \(verb2) in the \(noun)."
+                
+                    madlibLabel.text = str
+            }
+        }
+    }
 
-
+    
+    
 }
 
